d3145d19759341c3c611e406347f78c7e552c875
aakter <aakter002@citymail.cuny.edu>
2021-03-09 18:02:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,26 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    // Initialize Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // Set applicationId, and server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configure
+        // any network interceptors must b added with the Configuration Builder given this
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("instagram_my")
+                .clientKey("CodepathMoveFastParse")
+                .server("https://instagram_my.herokuapp.com/parse/").build());
+    }
+}
